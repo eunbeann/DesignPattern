a9259676bd72aefb1f06c379ab664cc66ca1e38d
Eunbeann <ds5bean23@gmail.com>
2023-03-09 11:44:55 +0900

    feat:practe01 - Book

--- DP_2023-1/src/ch01/practice/BookShelf.java
+++ DP_2023-1/src/ch01/practice/BookShelf.java
@@ -1,5 +1,27 @@
 package ch01.practice;
 
 public class BookShelf {
-    
+    private Book[] books; // 배열일 땐 변수 이름 복수로 주기 // 배열 선언
+    private int last = 0; // 책의 마지막 위치, 책 꽂고 뽑으려면 위치 변수 필요
+
+    public BookShelf(int maxsize) {
+        // 배열 생성 (배열은 선언과 생성 과정 필요)
+        this.books = new Book[maxsize]; // 배열의 특징 : 배열 생성 시 크기 결정, 속도는 빠르지만 생성시 크기 고정
+        // 생성될 때 사이즈 만들어서!! 일반화 될 수 있는지 고민하기
+    }
+
+    // 책을 꽂는 메소드
+    public void appendBook(Book book) {
+        // 인자로 들어온 책을 books라는 배열에 담아두기
+        this.books[last] = book;
+        last++;
+
+    }
+
+    // 책을 꺼내오는 메소드 // 인자로 인덱스 책 위치
+    public Book getBookAt(int index) {
+        return books[index];
+    }
+    // 책 개수 리턴 메소드
+
 }
--- DP_2023-1/src/ch01/practice/Main.java
+++ DP_2023-1/src/ch01/practice/Main.java
@@ -5,10 +5,31 @@ public class Main {
     public static void main(String[] args) {
         // 책 생성
         Book b1 = new Book("book1");
-        System.out.println(b1.getName());
+        Book b2 = new Book("book2");
+
+        // System.out.println(b1.getName()); 생성 확인
+
         // 책 부모 지정안하면 항상 오브젝트 상속 받음
         // snippet -> sysout
-        System.out.println();
+
+        // 책 꽂이 생성
+        BookShelf bs1 = new BookShelf(10);// 책 열권 꽂는 책꽂이
+
+        bs1.appendBook(b1);
+        // Book result1 = bs1.getBookAt(0); // b1이 0번째에 꽂혔을테니까 0번째에서 가져와라~
+        // System.out.println(result1.getName());
+
+        bs1.appendBook(b2);
+        // Book result2 = bs1.getBookAt(0); // b1이 0번째에 꽂혔을테니까 0번째에서 가져와라~
+        // System.out.println(result2.getName());
+
+        // for 루프를 이용해서 책 이름 출력하기
+        // ~책 개수보다 작거나 같을때까지 돌리기
+        for (int i = 0;; i++) {
+            System.out.println(bs1.getBookAt(i).getName());
+            // bs1.getBookAt(i)의 리턴 타입이 Book이니까
+
+        }
     }
 
 }
