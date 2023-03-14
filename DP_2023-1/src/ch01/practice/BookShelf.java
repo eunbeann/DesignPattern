720efc491107678568e1b85dfa9f1147c4a95867
Eunbeann <ds5bean23@gmail.com>
2023-03-14 10:14:36 +0900

    feat: 3/14 Iterator

--- DP_2023-1/src/ch01/practice/BookShelf.java
+++ DP_2023-1/src/ch01/practice/BookShelf.java
@@ -1,8 +1,14 @@
 package ch01.practice;
 
-public class BookShelf {
+//JDK 제공 패키지에 있는 아이터레이터를 이용한다.
+import java.util.Iterator;
+
+//JDK 제공 Iteralbe 인터페이스 쓰자
+//Iterable<Book> : Bookshelf는 반복이 가능한데, 각 원소의 타입은 책이다를의미함., 책을 돌아다니는 ㅏㅇ이터러블
+public class BookShelf implements Iterable<Book> {
     private Book[] books; // 배열일 땐 변수 이름 복수로 주기 // 배열 선언
-    private int last = 0; // 책의 마지막 위치, 책 꽂고 뽑으려면 위치 변수 필요
+//    책 꽂이는 책을 포함하니까
+    private int last = 0; // 정확히 말하면 다음에 꽂힐 책의 위치, 책의 마지막 위치, 책의 개수를 나타냄 책 꽂고 뽑으려면 위치 변수 필요
 
     public BookShelf(int maxsize) {
         // 배열 생성 (배열은 선언과 생성 과정 필요)
@@ -15,13 +21,35 @@ public class BookShelf {
         // 인자로 들어온 책을 books라는 배열에 담아두기
         this.books[last] = book;
         last++;
-
     }
 
     // 책을 꺼내오는 메소드 // 인자로 인덱스 책 위치
     public Book getBookAt(int index) {
         return books[index];
     }
+
     // 책 개수 리턴 메소드
+    public int getLength() {
+//        책 한 권 꽂힐 때 마다 하나씩 증가하는 last가 책을 가짐
+//        last-1이 될 수도 있음 -> 한권 꽂으면1이되고, 두권꽂으면2가 된다는 점 알고있기
+        return last;
+    }
+
+    // iterator 반환하는 메소드
+//    public BookShelfIterator iterator() {
+//    인터페이스 썼으니까 더 일반적으로 바꿔주기 : 프로그램은 일반적으로 쓰는 게 좋으니까 책을 돌아다니느 아이터러블
+    // 부모 즉 인터페이스에 정의된 메소드를 구현했기 때문에 @Override붙이면 > 부모에 선언된 것을 오버라이드 한 것이다 라고 표시함.
+    // 표시해두면 좋은 점: 위 주석과 구분되어 부모쪽에서 온 메소드구나를 알 수 있음
+    // @ : 어노테이션 지정 기호 > 이 경우에는 뭔가 표시를 해주는 역할을 함.
+    @Override
+    public Iterator<Book> iterator() {
+//        북쉘프아이터레이터 반환
+        return new BookShelfIterator(this); //현재 책꽂이와 iterator 연결됨.
+//        bookshelf마다 아이터레이터 반환해주는 거 만들면 아이터레이터라만 일함.
+//        책 꽂이 넣어줘야지 아이터레이터와 연결됨. => 현재 내 ㄱ책꽂이랑 연결하려고 this 넣어줌 this : 현재 객체
+    }
+
+
+
 
 }
--- DP_2023-1/src/ch01/practice/Main.java
+++ DP_2023-1/src/ch01/practice/Main.java
@@ -1,5 +1,7 @@
 package ch01.practice;
 
+import java.util.Iterator;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -15,6 +17,7 @@ public class Main {
         // 책 꽂이 생성
         BookShelf bs1 = new BookShelf(10);// 책 열권 꽂는 책꽂이
 
+//        책 꽂기
         bs1.appendBook(b1);
         // Book result1 = bs1.getBookAt(0); // b1이 0번째에 꽂혔을테니까 0번째에서 가져와라~
         // System.out.println(result1.getName());
@@ -23,13 +26,43 @@ public class Main {
         // Book result2 = bs1.getBookAt(0); // b1이 0번째에 꽂혔을테니까 0번째에서 가져와라~
         // System.out.println(result2.getName());
 
-        // for 루프를 이용해서 책 이름 출력하기
-        // ~책 개수보다 작거나 같을때까지 돌리기
-        for (int i = 0;; i++) {
-            System.out.println(bs1.getBookAt(i).getName());
-            // bs1.getBookAt(i)의 리턴 타입이 Book이니까
+//        두 권의 책 꽂아서 아래에서 책 이름 출력해보기
 
+        // i가 책 개수보다 작거나 같을때까지 돌리기
+        System.out.println("for 루프를 이용해서 책 이름 출력하기");
+        for (int i = 0; i<bs1.getLength(); i++) {
+            System.out.println(bs1.getBookAt(i).getName());
+        }
+            // bs1.getBookAt(i)의 리턴 타입이 Book이니까
+//            exception 발생, mainmethod에서 발생하는 nullpointexception 발생 :
+//            우리가 프로그래밍을 하면, 객체 "변수"가 있고, 객체 변수(주소를)를 따라가면 진짜 객체가 있음 nullpointexceoption은 변수 따라갔더니 아무것도 없는 경우임
+//            결국은 객체가 없다~! 객체가 없는데 일해라 하는 경우를 말함. 우리가 잘 생각해보면 어디까지 돌아갈지 할 때bs1.getLength()에서 이퀄의 필요여부 잘 생각해봐야함.
+//            0,1일 때 두 권을 꽂음. 자바에선 -1, =여부를 신경써야함. i가 0일때, 1일때 책 두 권인데 i가 2일엔 책 없음. i가 2일때 인덱스 2에 해당하는 값은 없는데 i를 2까지 돌리니까 이퀄을 빼던가, getLength()-1 을 해서 해결한다. 이젠 널포인트익셉션 안뜸.
+
+        // 확장 for 루프를 이용해서 책 이름 출력하기
+//        for문 콜론 오른쪽엔 집합체 왼쪽에는 원소하나씩 꺼내올 떄사용하는 b라는 변수
+        System.out.println("확장 for문 사용하기");
+        for ( Book b: bs1) {
+           System.out.println(b.getName());  // 책 한권씩 접근
+        }
+
+
+
+//            iterator를 이용해서 책 이름 출력하기 : 클라이언트먼저 생각해보자
+//            1. 아이터레이터 얻기, 2. 아이터레이터 이용해서 원소 얻어오기
+            // iteraotor 얻기
+//            책 꽂잉
+            System.out.println("<book list using iterator>");
+//        자바 프로그래밍씨 타입 잘 맞춰줘야함. 아이터레이터 타입 바꿨으니까 바뀐거로 다시 해줘야함
+        Iterator<Book> iterator= bs1.iterator();
+//           BookShelfIterator iterator = bs1.iterator();
+//            책꽂이에 아이터레이터를 얻어가는 메소드랑, 아이터레이터를 하나 더 만들어야함. 아이터레이터에 넥스트 또 구현해줘야함.
+            // iterator 이용해서 원소 얻어옴
+//                    아이터레이터 항상 아래와 같은 형식, 책 더 잇으면 책 억어옴
+            while(iterator.hasNext()) {
+                Book b = iterator.next();
+//                클라이언트는 아이터레이터한테 넥스트만 호출하면 다음 원소 얻어짐
+                System.out.println(b.getName());
+            }
         }
     }
-
-}
